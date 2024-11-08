eb4209507b0ee3f4eb95ac3210c653169d9912d0
Natnael Alemayehu <se.natnael.alemayehu@gmail.com>
2024-11-08 22:09:35 +0300

    Authentication with session storaga on redis implemented

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -2,26 +2,18 @@ package middleware
 
 import (
 	"net/http"
-	"os"
 
-	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
-	"github.com/natnael-alemayehu/recipes-api/handlers"
 )
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		tokenValue := c.GetHeader("Authorization")
-		claims := &handlers.Claims{}
-
-		tkn, err := jwt.ParseWithClaims(tokenValue, claims, func(t *jwt.Token) (interface{}, error) {
-			return []byte(os.Getenv("JWT_SECRET")), nil
-		})
-		if err != nil {
-			c.AbortWithStatus(http.StatusUnauthorized)
-		}
-		if tkn == nil || !tkn.Valid {
-			c.AbortWithStatus(http.StatusUnauthorized)
+		session := sessions.Default(c)
+		sessionToken := session.Get("token")
+		if sessionToken == nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"Message": "Not logged"})
+			c.Abort()
 		}
 
 		c.Next()
